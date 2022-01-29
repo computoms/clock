38ea1bd52669a60686dfa197601b5be81da37b6f
thomas <thomas@computoms.com>
2022-01-29 21:30:48 +0100

    Allows to show the last task when calling clock_report.py without any arguments

--- clock_report.py
+++ clock_report.py
@@ -8,13 +8,17 @@ from clock_framework.filters import DateFilter
 
 # Options
 class ReportOptions:
-    today = False
-    arguments = []
+    def __init__(self):
+        self.today = False
+        self.show_current = False
+        self.arguments = []
 
     def parse(self, argv):
         start_index = 1
-        if len(argv) > 0 and argv[1] == 'show':
+        if len(argv) > 1 and argv[1] == 'show':
             start_index = 2
+        if len(argv) == 1:
+            self.show_current = True
 
         for arg in argv[start_index:]:
             if '--today' in arg:
@@ -26,7 +30,7 @@ class ReportOptions:
         filters = []
         if len(self.arguments) > 0:
             filters.append(TagFilter(self.arguments))
-        if self.today:
+        if self.today or self.show_current:
             filters.append(DateFilter(datetime.datetime.today()))
         return filters
 
@@ -36,6 +40,9 @@ options.parse(sys.argv)
 collection = TaskCollection.from_file('./clock.txt')
 for filter in options.get_filters():
     collection = collection.filter(filter)
+
+if options.show_current and len(collection.tasks) > 1:
+    collection.tasks = collection.tasks[-1:]
 
 report = TaskReport(collection)
 report.show(len(options.arguments))
