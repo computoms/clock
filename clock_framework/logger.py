d046b2afccfb90459897fe889159072d4a75bebb
Thomas Beck <beck@imina.ch>
2022-01-31 15:25:32 +0100

    Finds correct position of the entry when using the --at option

--- clock_framework/logger.py
+++ clock_framework/logger.py
@@ -37,13 +37,31 @@ class ClockLogger:
             return
         new_description = self.lines[-1][0:5] + ' ' + description + '\n'
         self.lines[-1] = new_description
+        print('Edited current entry: ' + self.lines[-1])
+
+    # Finds position of entry given the 'at' hour (today)
+    def find_position(self, at):
+        found_today = False
+        at_time = DateTimeUtils.parse_time(date.today(), at)
+        index = 0
+        for line in self.lines:
+            if DateTimeUtils.is_date(line) and DateTimeUtils.get_date(line).date() == date.today():
+                found_today = True
+            elif found_today and not DateTimeUtils.is_date(line):
+                current_time = DateTimeUtils.parse_time(date.today(), line[0:5])
+                if current_time > at_time:
+                    return index
+            
+            index = index + 1
+        return index
 
     def add(self, at, description):
         if not self.date_exists():
             self.add_today()
-        
+
         new_line = at + ' ' + description + '\n'
-        self.lines.append(new_line)
+        self.lines.insert(self.find_position(at), new_line)
+        print('Added new entry: ' + new_line)
 
 class ClockReader:
     def __init__(self):
